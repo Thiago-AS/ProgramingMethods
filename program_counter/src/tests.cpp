d4e37410b65db297990ab838b98aecb5364af2be
Thiago-AS <thiagos.araujo09@gmail.com>
2018-10-11 13:16:55 -0300

    Adding error tests

--- program_counter/src/tests.cpp
+++ program_counter/src/tests.cpp
@@ -35,8 +35,8 @@ TEST_CASE("Alocar classe FileHandler") {
         }
 
         SECTION("Contar quantidade de linhas de codigo") {
+          int valid_lines = 0;
           try {
-            int valid_lines = 0;
             valid_lines = file_handler->GetValidLines();
             REQUIRE(valid_lines == 15);
           } catch(const char* error) {
@@ -50,6 +50,48 @@ TEST_CASE("Alocar classe FileHandler") {
       bool close_correctly = false;
       close_correctly = file_handler->CloseFile();
       REQUIRE(close_correctly == true);
+    }
+  }
+
+  SECTION("Definir arquivo incorretamente") {
+    bool set_correctly = false;
+    set_correctly = file_handler->SetFile("arquivo_inexistente.cpp");
+    REQUIRE(set_correctly == false);
+
+    SECTION("Invocar metodo GetFileLines sem arquivo") {
+      int file_lines = 0;
+      try {
+        file_lines = file_handler->GetFileLines();
+      } catch(const char* error) {
+        REQUIRE(file_handler->file.is_open() == false);
+        REQUIRE(file_lines == 0);
+      }
+    }
+
+    SECTION("Invocar metodo GetInvalidLines sem arquivo") {
+      int invalid_lines = 0;
+      try {
+        invalid_lines = file_handler->GetInvalidLines();
+      } catch(const char* error) {
+        REQUIRE(file_handler->file.is_open() == false);
+        REQUIRE(invalid_lines == 0);
+      }
+    }
+
+    SECTION("Invocar metodo GetValidLines sem arquivo") {
+      int valid_lines = 0;
+      try {
+        valid_lines = file_handler->GetValidLines();
+      } catch(const char* error) {
+        REQUIRE(file_handler->file.is_open() == false);
+        REQUIRE(valid_lines == 0);
+      }
+    }
+
+    SECTION("Invocar metodo CloseFile sem arquivo") {
+      bool close_correctly = false;
+      close_correctly = file_handler->CloseFile();
+      REQUIRE(close_correctly == false);
     }
   }
   delete file_handler;
