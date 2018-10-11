cc946488dbd9a4a59e29c0f9ad93b97a34255170
Thiago-AS <thiagos.araujo09@gmail.com>
2018-10-11 10:32:09 -0300

    Class alocation test

--- program_counter/src/tests.cpp
+++ program_counter/src/tests.cpp
@@ -1,1 +1,12 @@
+/**
+ *  @copyright
+ */
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+#include "../include/file_handler.h"
 
+TEST_CASE("Alocar classe FileHandler") {
+  FileHandler file_handler = nullptr;
+  file_handler = new FileHandler;
+  REQUIRE(file_handler != nullptr);
+}
