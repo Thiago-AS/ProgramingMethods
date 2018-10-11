03e5c513f236f55d5ef883a892fe6496da642032
Thiago-AS <thiagos.araujo09@gmail.com>
2018-10-11 10:46:15 -0300

    Test if file set

--- program_counter/src/tests.cpp
+++ program_counter/src/tests.cpp
@@ -9,4 +9,10 @@ TEST_CASE("Alocar classe FileHandler") {
   FileHandler* file_handler = nullptr;
   file_handler = new FileHandler;
   REQUIRE(file_handler != nullptr);
+
+  SECTION("Definir arquivo no atributo de classe") {
+    bool set_correctly;
+    file_handler->SetFile("sample_file.cpp");
+    REQUIRE(set_correctly == true);
+  }
 }
